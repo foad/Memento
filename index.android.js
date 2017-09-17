b33fca47efab4e6c227f0c7c40aed7471ea36c21
Dan Foad <danielfoad@gmail.com>
2017-09-18 00:15:31 +0100

    Added topbar

--- index.android.js
+++ index.android.js
@@ -1,53 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+class TopBar extends Component {
+    render() {
+        return (
+            <View style={styles.topbar}>
+                <Text style={styles.topbar__title}>M E M E N T O</Text>
+            </View>
+        );
+    }
+}
+
 export default class Memento extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TopBar />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#EFEFEF',
+    },
+    topbar: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        height: 64,
+        alignSelf: 'stretch',
+        elevation: 5,
+    },
+    topbar__title: {
+        fontFamily: 'sans-serif-thin',
+        fontSize: 18,
+    }
 });
 
 AppRegistry.registerComponent('Memento', () => Memento);
